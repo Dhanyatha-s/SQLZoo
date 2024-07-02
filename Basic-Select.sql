4d27b968c1141a5e3e4c2b91c8b5f3f241a74a97
Dhanyatha S <95542660+Dhanyatha-s@users.noreply.github.com>
2024-07-02 16:19:55 +0530

    Update Basic-Select.sql

--- Basic-Select.sql
+++ Basic-Select.sql
@@ -1,4 +1,15 @@
-'
-SELECT population FROM world
-  WHERE name = 'Germany'
-`
+-- 1. The example uses a WHERE clause to show the population of 'France'. 
+-- Note that strings should be in 'single quotes';
+-- Modify it to show the population of Germany'
+
+    SELECT population FROM world
+      WHERE name = 'Germany'
+
+--2. Show the name and the population for 'Sweden', 'Norway' and 'Denmark'.
+    select name, population from world
+    where name in ('Sweden', 'Norway', 'Denmark');
+
+--3 Which countries are not too small and not too big?
+--  show the country and the area for countries with an area between 200,000 and 250,000.
+    select name, area from world
+    where area between '200000' and '250000';
